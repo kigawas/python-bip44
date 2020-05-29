b7005749fddf1f9f7b8b602f9c2d46f519fcc3b7
Weiliang Li <to.be.impressive@gmail.com>
2020-05-29 13:35:52 +0900

    Update bip32

--- bip44/wallet.py
+++ bip44/wallet.py
@@ -1,9 +1,8 @@
-from typing import Tuple, Iterable
+from typing import Tuple, Iterable, Union
 
 from bip32 import BIP32, HARDENED_INDEX
 from mnemonic import Mnemonic
 
-from bip44.utils import parse_path
 
 COIN_PATHS = {
     "BTC": (44 + HARDENED_INDEX, HARDENED_INDEX),
@@ -21,21 +20,22 @@ class Wallet:
         self._seed = _Mnemonic.to_seed(mnemonic)
         self._bip32 = BIP32.from_seed(self._seed)
 
-    def _derive_secret(self, path: Iterable[int]) -> bytes:
+    def _derive_secret(self, path: Union[str, Iterable[int]]) -> bytes:
         return self._bip32.get_privkey_from_path(path)
 
-    def _derive_public(self, path: Iterable[int]) -> bytes:
+    def _derive_public(self, path: Union[str, Iterable[int]]) -> bytes:
         return self._bip32.get_pubkey_from_path(path)
 
     def derive_secret_key(self, path: str) -> bytes:
-        return self._derive_secret(parse_path(path))
+        return self._derive_secret(path)
 
     def derive_public_key(self, path: str) -> bytes:
-        return self._derive_public(parse_path(path))
+        return self._derive_public(path)
 
     def derive_account(
         self, coin: str, account: int = 0, change: int = 0, address_index: int = 0
     ) -> Tuple[bytes, bytes]:
+        # m / purpose' / coin_type' / account' / change / address_index
         coin_path = COIN_PATHS[coin.upper()]
         account_path = (account + HARDENED_INDEX, change, address_index)
         path = coin_path + account_path
