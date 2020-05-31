526de5a01a28a762ff290350c2e838be4df87035
Weiliang Li <to.be.impressive@gmail.com>
2020-06-01 00:33:00 +0900

    Doc (#9)

--- bip44/utils.py
+++ bip44/utils.py
@@ -5,12 +5,14 @@ __all__ = ("keccak_256", "get_eth_addr")
 
 
 def keccak_256(b: bytes) -> bytes:
+    """Get keccak 256 hash from a bytes input."""
     h = _keccak_256()
     h.update(b)
     return h.digest()
 
 
 def get_eth_addr(pk: bytes) -> str:
+    """Get ETH address from a public key."""
     if len(pk) != 64:
         pk = PublicKey(pk).format(False)[1:]
     return f"0x{keccak_256(pk)[-20:].hex()}"
--- bip44/wallet.py
+++ bip44/wallet.py
@@ -15,7 +15,8 @@ __all__ = ("Wallet",)
 
 class Wallet:
     def __init__(self, mnemonic: str, language: str = "english"):
-        """BIP44 HD wallet with a master mnemonic
+        """BIP44 HD wallet with a master mnemonic.
+
         :param mnemonic (str): The master mnemonic to derive keys
         :param language (str, optional): The mnemonic's language, default: "english"
         """
@@ -30,15 +31,41 @@ class Wallet:
         return self._bip32.get_pubkey_from_path(path)
 
     def derive_secret_key(self, path: str) -> bytes:
+        """Derive secret key from path.
+
+        Args:
+            :param path (str): Key path, e.g. m/44'/0'/0'/0/0
+
+        Returns:
+            bytes: Bytes of secret/private key
+        """
         return self._derive_secret(path)
 
     def derive_public_key(self, path: str) -> bytes:
+        """Derive public key from path.
+
+        Args:
+            :param path (str): Key path, e.g. m/44'/0'/0'/0/0
+
+        Returns:
+            bytes: Bytes of public key, compressed format
+        """
         return self._derive_public(path)
 
     def derive_account(
         self, coin: str, account: int = 0, change: int = 0, address_index: int = 0
     ) -> Tuple[bytes, bytes]:
-        # m / purpose' / coin_type' / account' / change / address_index
+        """Derive secret and public key of account, following BIP44 standard like `m / purpose' / coin_type' / account' / change / address_index`.
+
+        Args:
+            :param coin (str): e.g. BTC or ETH
+            :param account (int, optional): Account index, each user should have one
+            :param change (int, optional): 0 for receiving payments and 1 for receiving changes, see BIP44 for details
+            :param address_index (int, optional): Should be increasing from 0
+
+        Returns:
+            Tuple[bytes, bytes]: Secret key and public key
+        """
         coin_path = COIN_PATHS[coin.upper()]
         account_path = (account + HARDENED_INDEX, change, address_index)
         path = coin_path + account_path
