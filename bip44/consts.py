20736f94ffcffa9d03da8895f450499b338fa194
Weiliang Li <to.be.impressive@gmail.com>
2020-06-11 00:26:18 +0900

    add doc

--- bip44/consts.py
+++ bip44/consts.py
@@ -3,6 +3,7 @@ from bip32 import HARDENED_INDEX
 
 
 def coin_path_by_index(index: int = 0) -> Tuple[int, int]:
+    """Full list is at https://github.com/satoshilabs/slips/blob/master/slip-0044.md#registered-coin-types"""
     return (44 + HARDENED_INDEX, index + HARDENED_INDEX)
 
 
