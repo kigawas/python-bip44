32609e4829ff6586a851cd75017a86457ec0a8c6
Weiliang Li <to.be.impressive@gmail.com>
2020-09-20 13:37:16 +0900

    Update utils.py (#25)
    * Update utils.py
    
    * Update utils.py

--- bip44/utils.py
+++ bip44/utils.py
@@ -3,22 +3,21 @@ from typing import Union
 from coincurve import PublicKey
 from sha3 import keccak_256 as _keccak_256
 
-__all__ = ("keccak_256", "get_eth_addr", "to_checksum_addr")
+__all__ = ("get_eth_addr", "to_checksum_addr")
 
 
 def keccak_256(b: bytes) -> bytes:
-    """Get keccak 256 hash from a bytes input."""
-
     h = _keccak_256()
     h.update(b)
     return h.digest()
 
 
 def to_checksum_addr(eth_addr: str) -> str:
-    """Convert eth address to eth checksum address
+    """
+    Convert eth address to eth checksum address.
+
     EIP 55: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md#implementation
     """
-
     address = eth_addr.lower().replace("0x", "")
     addr_hash = keccak_256(address.encode()).hex()
 
@@ -34,7 +33,6 @@ def to_checksum_addr(eth_addr: str) -> str:
 
 def get_eth_addr(pk: Union[str, bytes]) -> str:
     """Get ETH address from a public key."""
-
     pk_bytes = bytes.fromhex(pk) if isinstance(pk, str) else pk
 
     if len(pk_bytes) != 64:
